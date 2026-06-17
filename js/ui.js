257211bf0b836804bce5e089d21a7a9f5dcbe6ff
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 13:38:24 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,4 @@
 export const UI = {
-    toggleMenu: () => { document.getElementById('sidebar').classList.toggle('active'); },
-    updateAttendance: (name, time, isExp) => { if (!document.getElementById('row-'+name)) document.getElementById('attendance-body').innerHTML += `<tr id="row-${name}"><td>${name}</td><td>${isExp?'Có mặt':'Khách'}</td><td>${time}</td></tr>`; },
-    showInfoModal: (desc) => { document.getElementById('info-modal').style.display = 'block'; window.currentDescriptor = desc; },
-    captureFace: (v) => { const c = document.createElement('canvas'); c.width=200; c.height=200; c.getContext('2d').drawImage(v, 120, 40, 400, 400, 0, 0, 200, 200); return c.toDataURL(); },
+    updateAttendance: (name, isExp) => { document.getElementById('attendance-body').innerHTML += `<tr><td>${name}</td><td>${isExp?'Có mặt':'Khách'}</td></tr>`; },
     loadModels: async () => { const M = 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/'; await faceapi.loadTinyFaceDetectorModel(M); await faceapi.loadFaceLandmarkModel(M); await faceapi.loadFaceRecognitionModel(M); }
 };
