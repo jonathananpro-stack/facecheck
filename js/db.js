1effcdfca54f753b2a8af36325904e71448c1f65
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 13:50:16 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 export const db = {
-    initSession: (name, list) => { localStorage.setItem('current_session', JSON.stringify({id:Date.now(), expected:list, present:[]})); },
-    findMatch: (desc) => { const lib = JSON.parse(localStorage.getItem('library')||'[]'); const matcher = new faceapi.FaceMatcher(lib.map(p => new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(Object.values(p.descriptor))])), 0.4); const res = matcher.findBestMatch(desc); return res.label !== 'unknown' ? {name: res.label} : null; },
-    markPresence: (name) => { let s = JSON.parse(localStorage.getItem('current_session')); if (s && s.expected.includes(name) && !s.present.includes(name)) { s.present.push(name); localStorage.setItem('current_session', JSON.stringify(s)); return true; } return false; }
+    initSession: (n, l) => localStorage.setItem('current_session', JSON.stringify({expected: l, present: []})),
+    saveUser: (p) => { const lib = JSON.parse(localStorage.getItem('library')||'[]'); lib.push(p); localStorage.setItem('library', JSON.stringify(lib)); },
+    findMatch: (desc) => { const lib = JSON.parse(localStorage.getItem('library')||'[]'); const matcher = new faceapi.FaceMatcher(lib.map(p => new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(Object.values(p.descriptor))])), 0.4); const res = matcher.findBestMatch(desc); return res.label !== 'unknown' ? {name: res.label} : null; }
 };
