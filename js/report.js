2e0ae5375e442bde0b14e7d552f3b1fa9e506fe0
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 13:38:52 +0700

    Update report.js

--- js/report.js
+++ js/report.js
@@ -1,3 +1,1 @@
-export const Report = {
-    exportCSV: () => { const logs = JSON.parse(localStorage.getItem('attendance_logs') || '[]'); let csv = 'Tên,Thời gian\n'; logs.forEach(l => csv += `${l.name},${l.time}\n`); const a = document.createElement('a'); a.href = URL.createObjectURL(new Blob([csv], {type: 'text/csv'})); a.download = 'diem_danh.csv'; a.click(); }
-};
+export const Report = { export: () => console.log("Exporting data...") };
