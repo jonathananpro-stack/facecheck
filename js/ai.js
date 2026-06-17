56d4987cbc70af9032d31e5ab599392b2f4dd135
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 12:17:44 +0700

    Update ai.js

--- js/ai.js
+++ js/ai.js
@@ -1,8 +1,5 @@
 export async function detect(video) {
     const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 224 });
-    const result = await faceapi.detectSingleFace(video, options)
-        .withFaceLandmarks()
-        .withFaceDescriptor();
-    
+    const result = await faceapi.detectSingleFace(video, options).withFaceLandmarks().withFaceDescriptor();
     return result ? result.descriptor : null;
 }
