3a1bb702204778cc55cc7dc6f696e055003598f4
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 14:07:59 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 export const db = {
     initSession: (n, l) => localStorage.setItem('current_session', JSON.stringify({expected: l, present: []})),
-    saveUser: (p) => { const lib = JSON.parse(localStorage.getItem('library')||'[]'); lib.push(p); localStorage.setItem('library', JSON.stringify(lib)); },
-    findMatch: (desc) => { const lib = JSON.parse(localStorage.getItem('library')||'[]'); const matcher = new faceapi.FaceMatcher(lib.map(p => new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(Object.values(p.descriptor))])), 0.4); const res = matcher.findBestMatch(desc); return res.label !== 'unknown' ? {name: res.label} : null; }
+    saveUser: (p) => { const lib = JSON.parse(localStorage.getItem('lib')||'[]'); lib.push(p); localStorage.setItem('lib', JSON.stringify(lib)); },
+    findMatch: (desc) => { const lib = JSON.parse(localStorage.getItem('lib')||'[]'); const matcher = new faceapi.FaceMatcher(lib.map(p=>new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(p.descriptor)])), 0.4); const res = matcher.findBestMatch(desc); return res.label!=='unknown'?{name:res.label}:null; }
 };
