bcef963e0a559d7013d14b2b0a26f6334d6e713b
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 14:26:40 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 export const db = {
-    initSession: (n, l) => localStorage.setItem('current_session', JSON.stringify({expected: l, present: []})),
+    initSession: (l) => localStorage.setItem('ses', JSON.stringify({ex: l})),
     saveUser: (p) => { const lib = JSON.parse(localStorage.getItem('lib')||'[]'); lib.push(p); localStorage.setItem('lib', JSON.stringify(lib)); },
     findMatch: (desc) => { const lib = JSON.parse(localStorage.getItem('lib')||'[]'); const matcher = new faceapi.FaceMatcher(lib.map(p=>new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(p.descriptor)])), 0.4); const res = matcher.findBestMatch(desc); return res.label!=='unknown'?{name:res.label}:null; }
 };
