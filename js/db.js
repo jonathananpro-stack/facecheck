91c244d4ccd3b62fd71a11ff8a59825af2e0dc8f
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 15:20:03 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -5,10 +5,9 @@ export const db = {
     },
     findMatch: (desc) => { 
         const lib = JSON.parse(localStorage.getItem('lib')||'[]'); 
-        if(lib.length === 0) return null;
-        const matcher = new faceapi.FaceMatcher(lib.map(p => new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(p.descriptor)])), 0.5); 
-        const res = matcher.findBestMatch(new Float32Array(desc)); 
-        return res.label !== 'unknown' ? {name:res.label} : null; 
-    },
-    confirmIdentity: (name) => { console.log("Saved Attendance for: " + name); }
+        if(lib.length < 1) return null;
+        const matcher = new faceapi.FaceMatcher(lib.map(p=>new faceapi.LabeledFaceDescriptors(p.name, [new Float32Array(p.descriptor)])), 0.5);
+        const res = matcher.findBestMatch(new Float32Array(desc));
+        return res.label !== 'unknown' ? {name:res.label} : null;
+    }
 };
