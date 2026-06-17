dffc2b1605d43c3c0fb572318453fc14c6414a73
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-17 15:03:48 +0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,10 +1,13 @@
 export const UI = {
+    initCamera: async () => {
+        const video = document.getElementById('video');
+        video.srcObject = await navigator.mediaDevices.getUserMedia({video: true});
+        const M = 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/';
+        await faceapi.loadTinyFaceDetectorModel(M);
+    },
     showValidationOverlay: (name) => {
         const ov = document.getElementById('validation-overlay');
-        ov.innerHTML = `<p>Là ${name}?</p><button onclick="handleValidation(true)">ĐÚNG</button><button onclick="handleValidation(false)">SAI</button>`;
+        ov.innerHTML = `<p>Xác nhận ${name}?</p><button onclick="handleValidation(true)">ĐÚNG</button>`;
         ov.style.display = 'block';
-    },
-    showTrainModal: (img) => { document.getElementById('face-preview').src = img; document.getElementById('info-modal').style.display = 'flex'; },
-    cropFace: (v) => { const c = document.createElement('canvas'); c.width=100; c.height=100; c.getContext('2d').drawImage(v, 0, 0, 100, 100); return c.toDataURL(); },
-    loadModels: async () => { const M = 'https://cdn.jsdelivr.net/npm/@vladmandic/face-api/model/'; await faceapi.loadTinyFaceDetectorModel(M); await faceapi.loadFaceLandmarkModel(M); await faceapi.loadFaceRecognitionModel(M); }
+    }
 };
