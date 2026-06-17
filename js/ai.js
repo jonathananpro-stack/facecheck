563512347c1307b005ff5745b8852374105a7009
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-18 03:35:12 +0700

    Delete js/ai.js

--- js/ai.js
+++
@@ -1,4 +0,0 @@
-export async function detect(v) {
-    const res = await faceapi.detectSingleFace(v, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptor();
-    return res ? res.descriptor : null;
-}
