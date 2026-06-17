1418b8a5bd024e85ed6b9ebc809564afe61f8bd4
jonathananpro-stack <jonathanan.pro@gmail.com>
2026-06-18 04:56:08 +0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,14 @@
+export const db = {
+    save(data) {
+        const encrypted = btoa(JSON.stringify(data));
+        localStorage.setItem('core_' + data.timestamp, encrypted);
+    },
+    getAll() {
+        return Object.keys(localStorage)
+            .filter(k => k.startsWith('core_'))
+            .map(k => ({
+                time: k.split('_')[1],
+                data: JSON.parse(atob(localStorage.getItem(k)))
+            }));
+    }
+};
